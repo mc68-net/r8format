973e92aaa4a768b34bd143139109b00cd13bd751
Curt J. Sampson <cjs@cynic.net>
2020-08-20 00:34:12 +0900

    tolines: Implement parsetext()

--- pylib/tokmsbasic/toklines.py
+++ pylib/tokmsbasic/toklines.py
@@ -2,22 +2,30 @@ import  struct
 
 class TokLines:
     ''' A sequence of tokenized lines. The data for each line is typically
-        tokenized BASIC program text, but may be any string without $00
+        tokenized BASIC program text, but may be any string without 0x00
         bytes in it; this class handles only the line numbers and offsets.
 
         Symbol names such as `txttab` are taken from Microsoft or other
         documentation or source code, where possible.
 
         Attributes:
+        - `lines`: A dictionary mapping `int` line number to the line data
+           (without the terminating 0 byte).
         - `orig_text`: The original (pre-parse) text data this instance was
           instantiated with, if any (otherwise `None`). This should be the
           same as the result of `text()` if the original text was valid,
           `txttab` has not been changed from the value discovered by the
           parse, and no lines have been added, deleted or changed.
+        - `bad_offsets`: A `list` of bad offset values from the last
+          `parsetext()` run.
 
         Issues:
         - This currently assumes little-endian format, which is fine for
           8080 or 6502, but not for 6800.
+        - `parsetext()` assumes that an initial 0xFF byte in the data is
+          a file type byte from a disk save file. This could also be the
+          start of a 250 byte line, but I don't think that any versions of
+          MS-BASIC allow a line that long.
         - `maxlin` defaults to 65529, which is correct for MSX-BASIC and
           GW-BASIC, but not for early 6502 BASIC (63999).
     '''
@@ -44,13 +52,49 @@ class TokLines:
     def parsetext(self, text):
         ''' Parse the given binary `text` data into lines, adding them to
             the lines already held by this object. New lines with the same
-            line number as an existing line will overwrite the existing
-            line.
+            line number as an existing line will overwrite the existing line.
 
-            This returns `txttab`, which is calculated from the binary
-            data if it was not set on instantiation.
+            This returns the start address of `text` as calculated from
+            the data in `txt`; it does not change this instance's `txttab`
+
+            Parsing is done by scanning for the 0x00 terminator byte in the
+            line data. Except for the first offset, which is assumed
+            correct and used to calculate the starting address `txttab`,
+            offset values are ignored. However, they are checked against
+            what they're expected to be and a (lineno, expected offset,
+            actual offset) tuple is appended to the `bad_offsets` property
+            for each line that has an offset value not matching the length
+            of the data. (`bad_offsets` is cleared when `parsetext()` is
+            called so it will contain the bad offsets from only the most
+            recent call.)
         '''
-        return -1
+        self.bad_offsets = []
+
+        p = 0
+        if text[p] == 0xFF:     # If type byte (XXX or 250 char line!)
+            text = text[1:]     # is present, drop it.
+
+        def linelen():
+            ' Len of the entire line, including offset, lineno and terminator. '
+            return 2 + 2 + text[p+4:].find(b'\x00') + 1
+
+        #   Calculate txttab by subtracting the length of the first line,
+        #   calculated by finding its terminating 0x00 byte, from the
+        #   offset of the second line at the start of the text.
+        txttab = unle(text[p:]) - linelen()
+
+        while True:
+            offset = unle(text[p:])
+            if offset == 0:
+                break
+            lineno = unle(text[p+2:])
+            data = text[p+4 : p+linelen()-1]
+            self.setline(lineno, data)
+            p += linelen()
+            if p + txttab != offset:
+                self.bad_offsets.append((lineno, p+txttab, offset))
+
+        return txttab
 
     def setline(self, lineno, bs):
         ' Set line `lineno` of the text to the data `bs`. '
@@ -87,3 +131,8 @@ def le(n):
     ' Return _n_ as little-endian unsigned 16-bit int. '
     return struct.pack('<H', n)
 
+def unle(bs):
+    ''' Parse the first two bytes of _bs_ as a little-endian unsigned
+        16-bit int.
+    '''
+    return struct.unpack('<H', bs[0:2])[0]
