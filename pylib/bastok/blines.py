188be2e5897ea7e693eb343cc0b470e2f965fcc7
Curt J. Sampson <cjs@cynic.net>
2020-09-02 15:07:53 +0900

    bastok.blines: Allow multi-line numeric DATA statements
    Now BASIC lines must have "code" on them (anything that doesn't look like a
    numeric DATA statement value, really), so we no longer allow "empty" lines
    with only a line number, but BASIC doesn't allow that anyway.

--- pylib/bastok/blines.py
+++ pylib/bastok/blines.py
@@ -2,15 +2,22 @@ import  re
 
 def blines(plines, commentchar='‖'):
     ''' Given a list of "physical" lines `plines` from an expanded BASIC
-        file, remove all expanded BASIC comments  and join together plines
-        that form a single BASIC line, starting with a line number
-        and ending at the next pline that starts with a line number.
+        file, remove all expanded BASIC comments  and join together plines,
+        ignoring leading and trailing spaces, that form a single BASIC
+        line.
 
         "Expanded" BASIC comments start at `commentchar` and continue to
         EOL; any spaces in front of them `commentchar` are also removed.
+
+        A pline is considered to start a BASIC line if it starts with a
+        decimal number and is followed at least one non-numeric character
+        that is not a comma. This allows for continuation lines for DATA
+        statements of numeric values, e.g., `DATA 12` followed by `34` or
+        `34, 56`.
     '''
+
     #   BASIC lines start with a line number followed by a space
-    BSTART = re.compile(r'\d+')
+    BSTART = re.compile(r'\d+[^,\d]+')
 
     blines = []; bline = []
     for pline in plines:
